822ee35a94bcc0a2e6131a1c7183224667b7ee79
vchealy <vchealy@gmail.com>
2021-08-13 15:52:23 +0100

    first commit

---
+++ Day 1 - Band  Name Generator/band_name_generator.py
@@ -0,0 +1,19 @@
+# band_name_generator.py
+'''
+    A name generator using a couple of pieces of input from the user
+'''
+from os import system
+def name_generator():
+    system('cls')
+    print('Band Name Generator')
+    place = input('Name your favourite place: \n')
+    food = input('What is your favourite food? \n')
+    system('cls')
+    print()
+    print(place, food)
+    print()
+
+
+
+if __name__ == "__main__":
+    name_generator()
